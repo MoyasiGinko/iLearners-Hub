b4f3ea460bc5e9360d3d88f17b040ebf5d546ac0
moyasiginko <mrsiyam61@gmail.com>
2025-05-26 17:36:03 +0600

    Update cta-news.tsx

--- components/news-offers/news/cta-news.tsx
+++ components/news-offers/news/cta-news.tsx
@@ -13,24 +13,26 @@ interface CTANewsProps {
 
 const CtaNews: React.FC<CTANewsProps> = () => {
   return (
-    <div className="bg-transparent py-20 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-7xl px-4 mx-auto">
+    <div className="bg-transparent py-10 sm:py-16 md:py-20 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl px-2 sm:px-4 mx-auto">
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
-          className=" bg-gradient-to-r from-purple-500 to-blue-500 rounded-3xl p-8 text-white text-center"
+          className="bg-gradient-to-r from-purple-500 to-blue-500 rounded-2xl sm:rounded-3xl p-4 sm:p-6 md:p-8 text-white text-center"
         >
           <div className="max-w-3xl mx-auto">
-            <h2 className="text-3xl font-bold mb-4">Join Us Now</h2>
-            <p className="mb-6">
+            <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-2 sm:mb-4">
+              Join Us Now
+            </h2>
+            <p className="text-sm sm:text-md md:text-lg lg:text-xl mb-4 sm:mb-6 px-2 sm:px-4">
               Join iLearnersHub and unlock exciting learning opportunities!
               Register now and start your journey with us!
             </p>
-            <div className="flex flex-col sm:flex-row gap-4 justify-center">
+            <div className="flex flex-col sm:flex-row gap-3 sm:gap-4 justify-center">
               <CustomButton
                 text="Book a Lesson Now"
                 href="/register"
-                className="bg-gradient-to-r from-yellow-400 to-yellow-500 border-yellow-600 hover:bg-opacity-90"
+                className="text-sm sm:text-md md:text-lg lg:text-xl bg-gradient-to-r from-yellow-400 to-yellow-500 border-yellow-600 hover:bg-opacity-90"
               />
             </div>
           </div>
